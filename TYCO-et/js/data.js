bec93ced53fc1a5137931c2ec01a5623513daf7d
Yutong Xiong <34176722+yutongxiong@users.noreply.github.com>
2018-01-18 14:29:41 -0800

    Add files via upload

---
+++ TYCO-et/js/data.js
@@ -0,0 +1,33 @@
+// I put rooms here because, I see you are copying this into two js files.
+// remember, the page load from top to bottom.
+// as long as this file is before the other js
+// the other js can access variable in this file
+// this is the reason why when we use jquery, we need to include jquery.js link before our script
+// also, becare ful, because of this naming is very important so you don't overwrite this variable by accident in your scripts, unless you intend to
+var rooms = [
+    {
+        name: "GALAXY FANTASY",
+        intro: "Space and galaxy theme, wonderful light effect. <br>King size bed, no window.",
+        image: "img/room1.jpg"
+    },
+    {
+        name: "LOST IN PARIS",
+        intro: "Painted wall, decorated with mirror. <br>Queen size bed, no window.",
+        image: "img/room2.jpg"
+    },
+    {
+        name: "SAMURAI & BUSHIDO",
+        intro: "Acient Japanese castle style decorated. <br>Monster size bed, City view.",
+        image: "img/room3.jpg"
+    },
+    {
+        name: "ABOVE THE CLOUD",
+        intro: "Huge mirror on the ceiling,  painted wall. <br>Queen size bed, no window.",
+        image: "img/room4.jpg"
+    },
+    {
+        name: "THE ROULETTE",
+        intro: "Walls of mirror, advanced audio system. <br>Monster size bed, no window.",
+        image: "img/room5.jpg"
+    }
+];
---
+++ TYCO-et/js/popup.js
@@ -0,0 +1,31 @@
+// uncomment the console.log below, you will see that you get access to the rooms variable in data.js
+// console.log(rooms);
+
+/*as you can see in the html, there are a few .btn_more_info BUT
+* we are using $(this) here....this would help a lot because it only defines the .btn_more_info you clicked and not the others
+* */
+$('.btn_more_info').on('click', function(e){
+    e.preventDefault();
+    // uncomment below, and see what's in the console...
+    // console.log($(this));
+    // inside the console, you might see something like r.fn.init[button.btn_more_info]
+    // click on it then you'll see something like 0:button.btn_more_info
+    // either click on it, or just highlight it
+    // if you click on it, it'll bring you to the html and show which button you just clicked,
+    // if you highlight it, you should be able to see which button you clicked too but won't bring you to html
+    $(this).next().show();  // ok now, since $(this) is the button, in html, what's after the button? it's the modal you want to display, show() means display: block in css
+});
+
+$('.close').on('click', function(e){
+    e.preventDefault();
+    // this is pretty similar to the above on click function
+    // but using closest() meaning it will go up the html dom tree and find the closest dom that has class modal, then hide() =  display: none
+    $(this).closest('.modal').hide();
+});
+
+$('.btn_book').on('click', function(e){
+    e.preventDefault();
+    
+    $(this).find('.display_box').show();  
+});
+console.log($(this).find('.display_box'));
