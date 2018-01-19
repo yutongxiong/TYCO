a1b34ff945851e57343e19214257b14a24dc54fb
et4891 <et1103@hotmail.com>
2018-01-18 16:10:34 -0800

    added id to json

--- TYCO-et/js/data.js
+++ TYCO-et/js/data.js
@@ -6,26 +6,31 @@
 // also, becare ful, because of this naming is very important so you don't overwrite this variable by accident in your scripts, unless you intend to
 var rooms = [
     {
+        id: 1,
         name: "GALAXY FANTASY",
         intro: "Space and galaxy theme, wonderful light effect. <br>King size bed, no window.",
         image: "img/room1.jpg"
     },
     {
+        id: 2,
         name: "LOST IN PARIS",
         intro: "Painted wall, decorated with mirror. <br>Queen size bed, no window.",
         image: "img/room2.jpg"
     },
     {
+        id: 3,
         name: "SAMURAI & BUSHIDO",
         intro: "Acient Japanese castle style decorated. <br>Monster size bed, City view.",
         image: "img/room3.jpg"
     },
     {
+        id: 4,
         name: "ABOVE THE CLOUD",
         intro: "Huge mirror on the ceiling,  painted wall. <br>Queen size bed, no window.",
         image: "img/room4.jpg"
     },
     {
+        id: 5,
         name: "THE ROULETTE",
         intro: "Walls of mirror, advanced audio system. <br>Monster size bed, no window.",
         image: "img/room5.jpg"
